d3b7521ea233c84e7ee814f7562cb5cd04037cfd
Robasban <rohanbastebania@gmail.com>
2025-09-26 20:12:40 -0400

    commit test

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("Hello AI in Healthcare")
-v
+import pandas as pd
+from sklearn.datasets import load_iris
+
+# load dataset
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df["species"] = pd.Categorical.from_codes(iris.target, iris.target_names)
+
+print(df.head())
